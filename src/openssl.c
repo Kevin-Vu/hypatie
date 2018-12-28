f81ef4b1755d75787f76e5a4f88d0c414993eee0
VU Kévin <kevin.vu@hotmail.fr>
2018-12-28 12:41:06 +0100

    Update openssl.c

--- src/openssl.c
+++ src/openssl.c
@@ -203,11 +203,6 @@ char * md5_hash_from_file(const char * filename)
     {
         MD5_Update(&c, buf, bytes);
         bytes=read(filedes, buf, BUFSIZE);
-        if(bytes == -1 || bytes != BUFSIZE)
-        {   
-            fclose(filedes);
-            return NULL;
-        }
     }
 
     MD5_Final(md5, &c);
